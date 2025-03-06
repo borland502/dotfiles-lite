521cd1bb9c3312c6894354f4391c562a8f00ba06
Jeremy Hettenhouser <jhettenh@gmail.com>
2025-03-06 13:01:14 -0500

    Added setup for plugins needing configuration

--- install.sh
+++ install.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+# cult of the xdg base directory
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:=$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:=$HOME/.local/share}"
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:=$HOME/.cache}"
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:=$HOME/.local/run/user/$(id -u)}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:=$HOME/.local/state}"
+
+# Create the directories
+for path in "$XDG_CONFIG_HOME" "$XDG_DATA_HOME" "$XDG_CACHE_HOME" "$XDG_RUNTIME_DIR" "$XDG_STATE_HOME"; do
+  mkdir -p "$path"
+done
+
 PATH="$HOME/.local/bin:/home/linuxbrew/.linuxbrew/bin:/opt/linuxbrew/bin:$PATH"
 
 # Install the dependencies
-
 rsync -u "config/zsh/.zshrc" "$HOME/"
 rsync -u "config/zsh/.zimrc" "$HOME/"
-
 
 ## Homebrew
 if ! [[ $(command -v brew) ]]; then
